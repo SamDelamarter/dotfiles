8fef3e9c3a2ae21c9887214f6e839860ffe413e9
danmikita <danmikita@gmail.com>
2018-12-03 13:55:09 -0500

    Correcting file path in setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 ln -s -f ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/tmux/.tmux.conf.local ~/.tmux.conf.local
-ln -s -f ~/dotfiles/nvim ~/.configs/nvim
-ln -s -f ~/dotfiles/alacritty ~/.configs/alacritty
+ln -s -f ~/dotfiles/nvim ~/.config/nvim
+ln -s -f ~/dotfiles/alacritty ~/.config/alacritty
 ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
