5d0f2d314436fde76da20575412918ae33e2973a
danmikita <danmikita@gmail.com>
2019-01-07 07:42:55 -0500

    Adding intellij settings and gitconfig

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ln -s -f ~/dotfiles/alacritty ~/.config/alacritty
 ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/idea/.ideavimrc ~/.ideavimrc
 ln -s -f ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
