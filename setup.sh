1e1b66ddaaa4ffe339824e298047619121965c11
Mikita <dan90374@sh149602.spectrum-health.org>
2019-01-07 07:37:05 -0500

    Adding changes to Brewfile and setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s -f ~/dotfiles/nvim ~/.config/nvim
 ln -s -f ~/dotfiles/alacritty ~/.config/alacritty
 ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/idea/.ideavimrc ~/.ideavimrc
+ln -s -f ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
