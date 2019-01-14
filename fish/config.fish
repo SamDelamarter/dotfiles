45ad1609a8ccf6cc585a2aa8595a6ecafe18fc1f
danmikita <danmikita@gmail.com>
2019-01-14 10:48:11 -0500

    Update paths.

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+set PATH $PATH ~/code/bin ~/code/bin/homebin
+
+set GOPATH ~/code/
+
 # BobTheFish Configs
 set -g theme_nerd_fonts yes
 #set -g theme_display_k8s_context yes
@@ -10,7 +14,7 @@ set -g theme_display_vi yes
 fish_vi_key_bindings
 
 # Aliases
-alias ong="oc login https://openshift.ong.spectrum-health.org -u=dan90374"
+alias ong="oc login https://openshift.ong.spectrum-health.org -u=sam99676"
 alias ms="oc login (minishift ip):8443 -u=admin -p=admin"
 alias amq-run="docker run -d --name local_amq  -p 61616:61616 -p 8161:8161 rmohr/activemq"
 alias amq-start="docker start local_amq"
@@ -19,3 +23,5 @@ alias amq-rm="docker rm local_amq"
 # you can hit the console on http://localhost:8161/admin
 # admin/admin to login
 
+# External Sources
+# source ~/code/src/github.com/troyxmccall/wd/wd
