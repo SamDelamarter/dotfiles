40e968be5d618610c35f91910e82f2a9bd002f88
danmikita <danmikita@gmail.com>
2019-09-10 10:59:30 -0400

    Adding new fzf functions

--- fish/config.fish
+++ fish/config.fish
@@ -29,8 +29,10 @@ alias refresh="source ~/dotfiles/fish/config.fish"
 alias ms="oc login (minishift ip):8443 -u=admin -p=admin"
 alias fishconfig="nvim ~/dotfiles/fish/config.fish"
 alias vimconfig="nvim ~/dotfiles/nvim/init.vim"
-alias diff="git difftool"
+# alias diff="git difftool"
 alias vim="nvim"
+
+alias fzfcmd="fzf --ansi -i -1 --height=50% --reverse -0 --inline-info --border"
 
 # Maven aliases
 alias mvnci="mvn clean install"
@@ -42,6 +44,42 @@ alias wg="cd ~/git"
 alias skynet="cd ~/git/skynet"
 alias todos="cd ~/git/todos"
 alias go-furl="cd ~/go/src/github.com/danmikita/furl"
+alias jenk="ssh -t dmikita@rhokvtstdmsk01 'sudo su -'"
 
-# The next line updates PATH for the Google Cloud SDK.
-# if [ -f '/Users/dan90374/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/dan90374/Downloads/google-cloud-sdk/path.fish.inc'; end
+function fco -d "Fuzzy-find and checkout a branch"
+  git branch --all | grep -v HEAD | string trim | fzfcmd | read -l result; and git checkout "$result"
+end
+
+function fcoc -d "Fuzzy-find and checkout a commit"
+  git log --pretty=oneline --abbrev-commit --reverse | fzfcmd --tac +s -e | awk '{print $1;}' | read -l result; and git checkout "$result"
+end
+
+function bcd -d 'cd backwards'
+	pwd | awk -v RS=/ '/\n/ {exit} {p=p $0 "/"; print p}' | tail -r | fzfcmd +m --select-1 --exit-0 $FZF_BCD_OPTS | read -l result
+	[ "$result" ]; and cd $result
+	commandline -f repaint
+end
+
+function h -d 'cd to one of the previously visited locations'
+	# Clear non-existent folders from cdhist.
+	set -l buf
+	for i in (seq 1 (count $dirprev))
+		set -l dir $dirprev[$i]
+		if test -d $dir
+			set buf $buf $dir
+		end
+	end
+	set dirprev $buf
+	string join \n $dirprev | tail -r | sed 1d | fzfcmd +m --tiebreak=index --toggle-sort=ctrl-r $FZF_CDHIST_OPTS | read -l result
+	[ "$result" ]; and cd $result
+	commandline -f repaint
+end
+
+function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
+  ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzfcmd | read -l result; and ssh "$result"
+end
+
+function fh -d "Fuzzy-find ssh host via ag and ssh into it"
+  history | fzfcmd | read -l result
+  commandline -r $result
+end
